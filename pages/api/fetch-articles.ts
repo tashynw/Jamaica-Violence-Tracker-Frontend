70bfe073b24eefc948d97b2e1044617fec0baf4e
unknown <wallacetashyn@gmail.com>
2023-08-09 17:58:51 -0500

    vercel deploy setup

---
+++ pages/api/fetch-articles.ts
@@ -0,0 +1,12 @@
+import axios from "@/local-api/axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { data } = await axios.get(`/cron`);
+
+  console.dir(data);
+  return res.status(200).json({ message: `CRON Successful` });
+}
